98855574545a81a710435fb2e209ea953f18871c
jmcb <joelsgp@protonmail.com>
2022-07-26 20:39:56 +0100

    Need to stop making sh changes

--- go.sh
+++ go.sh
@@ -3,5 +3,7 @@ dir="$HOME/src/SYS1"
 assembler="$dir/assembler_v1d/Linux/simpleCPUv1d_as.py"
 pattern="*.asm"
 files=( $pattern )
-python2 "$assembler" -i "${files[0]}" -o "code" \
+source="${files[0]}"
+echo "$source"
+python2 "$assembler" -i "$source" -o "code" \
 && cp -vu code.dat "$dir/modified_simpleCPU_v1d/"
