d3ab4ffbf535ae4e51400b047f0edaf25eaf7492
jmcb <joelsgp@protonmail.com>
2022-07-26 21:03:25 +0100

    Make a start, implement reverse

--- Q2/encrypt.asm
+++ Q2/encrypt.asm
@@ -1,1 +1,35 @@
+# subroutine
+# reverses the lower 8 bits in the ra register
+# the reversed word is grown in the rb register
+# the rc register counts a bit going up
+# rd is also used for checking when loop ends
+# todo cha cha slide joke
+reverse:
+    move rc 1  # 2^i
+    revloop:
+        rol ra
+        jumpc ycarry
+        jump fcarry
+        ycarry:
+            xor rb rc
+        fcarry:
+        rol rc
+        move rd rc
+        sub rd 128
+        jumpnz revloop
+    move rb ra
+    ret
+
+maskl:
+    .data 240
+maskr:
+    .data 15
+left:
+    .data 0
+right:
+    .data 0
+
+# 16b pixel starts in ra
+feistel:
+    move rb ra
 
