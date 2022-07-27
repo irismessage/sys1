8e67ee14e1f329d30e775b3cfe3d7167a69c15e3
jmcb <joelsgp@protonmail.com>
2022-07-27 16:28:47 +0100

    Format divide.asm more

--- Q4/divide.asm
+++ Q4/divide.asm
@@ -1,29 +1,28 @@
 start:
-  move RA 0x7F      # set dummy pixel values
-  call divide       # divide 0x7F/3=0x2A
-  store RA 0xFFF    # signal subroutine finished
+# 127 / 3 = 42.3333 ≈ 40
+    move RA 0x7F      # set dummy pixel values
+    call divide       # divide 0x7F/3=0x2A
+    store RA 0xFFF    # signal subroutine finished
 
-  move RA 0x0F      # set dummy pixel values
-  call divide       # divide 0x0F/3=0x05
-  store RA 0xFFF    # signal subroutine finished
+# 15 / 3 = 5
+    move RA 0x0F      # set dummy pixel values
+    call divide       # divide 0x0F/3=0x05
+    store RA 0xFFF    # signal subroutine finished
 
-  move RA XXX       # random value XXX assigned
-  call divide       # during testing
-  store RA 0xFFF    # signal subroutine finished
+#   move RA XXX       # random value XXX assigned
+    call divide       # during testing
+    store RA 0xFFF    # signal subroutine finished
 
 trap:
-  jumpu trap        # end
+    jumpu trap        # end
 
 
 # divides by three (3)
+# rounds down
 # argument: ra
 # return: ra
 divide:
-  ...               # add your code here
+    ret
 
-  ret
 
 data:
-  ...               # add your data/variables here
-
-
