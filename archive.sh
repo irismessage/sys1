b6298f872d5f7ca99315c8ad19e7727ef2b8867c
jmcb <joelsgp@protonmail.com>
2022-07-27 16:49:42 +0100

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -1,5 +1,5 @@
 cd ${0%/*}
 name="SYS1"
-git archive -v --prefix=$name/ -o zip/$name.zip HEAD .
+git archive -v -o zip/$name.zip HEAD .
 git bundle create zip/$name.bundle main
 zip -rv zip/$name.zip zip/$name.bundle
