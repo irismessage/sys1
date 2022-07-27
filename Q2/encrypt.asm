bb5821abeae9a241ae8f7ff6be700d069ce58511
jmcb <joelsgp@protonmail.com>
2022-07-27 15:56:05 +0100

    Try to fix feistel

--- Q2/encrypt.asm
+++ Q2/encrypt.asm
@@ -1,8 +1,9 @@
 jump start
 
 
+# 2^8
 revend:
-    .data 128
+    .data 256
 
 
 # subroutine
@@ -26,7 +27,6 @@ reverse:
         move ra rc
         subm ra revend  # ra used for checking loop end
         jumpnz takeitbacknowyall
-    move ra rb
     ret
 
 
@@ -42,24 +42,44 @@ roleight:
     rol ra
     ret
 
+lowmask:
+    .data 256
 pixel:
     .data 0
 nr:
     .data 0
 
+
+# example runthrough
+# px 1000011001100100
+# 3a = px[0:8]
+# 3a 10000110         <- L
+# 3b = px[8:16]
+# 3b         01100100 <- R
+# 3c = F(3b)
+# 3c         00100110
+# 3d = 3a xor 3c
+# 3d         10100000 -> NR
+# 3e = F(3d)
+# 3e         00000101
+# 3f = 3b xor 3e
+# 3f 11000001         -> NL
+
 # 16bit pixel starts in ra
 # see notation in 3-feistel.png
 feistel:
+# example: 
     store ra pixel
-    call reverse
-    move rb ra  # ~ rb <- 3c ~ lower bits are reversed in ra
+    call reverse # ~ rb <- 3c ~ lower bits are reversed in rb
     load ra pixel
-    call roleight  # ~ ra <- 3a ~ upper bits are in ra
+    call roleight
+    and ra 255 # ~ ra <- 3a ~ upper bits are in lower ra
     xor ra rb  # ~ ra <- 3d ~ first xor
     store ra nr
     call reverse  # ~ ra <- 3e ~ second reverse
     move rb ra
     load ra pixel
+    and ra 255
     xor ra rb # ~ 3f <- ra ~
     call roleight
     move rb ra
