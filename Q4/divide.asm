055fdaef298647f207c6c93a21cf19eb93e2f2d4
jmcb <joelsgp@protonmail.com>
2022-07-27 16:45:30 +0100

    Finish divide.asm

--- Q4/divide.asm
+++ Q4/divide.asm
@@ -1,17 +1,19 @@
 start:
 # 127 / 3 = 42.3333 ≈ 40
+# 40 = 0b00101000
     move RA 0x7F      # set dummy pixel values
     call divide       # divide 0x7F/3=0x2A
     store RA 0xFFF    # signal subroutine finished
 
 # 15 / 3 = 5
+# 5 = 0b00000101
     move RA 0x0F      # set dummy pixel values
     call divide       # divide 0x0F/3=0x05
     store RA 0xFFF    # signal subroutine finished
 
-#   move RA XXX       # random value XXX assigned
-    call divide       # during testing
-    store RA 0xFFF    # signal subroutine finished
+#    move RA XXX       # random value XXX assigned
+#    call divide       # during testing
+#    store RA 0xFFF    # signal subroutine finished
 
 trap:
     jumpu trap        # end
@@ -23,12 +25,18 @@ trap:
 # return: ra
 divide:
     move rb 0
+    move rd 0
     loop:
         add rb 1
         sub ra 3
-        jumpc end
+# return if ra is between 0 and 2 inclusive
+            jumpz end
+            move rc ra
+            sub rc 1
+            jumpz end
+            sub rc 1
+            jumpz end
         jump loop
     end:
-        sub rb 1
         move ra rb
     ret
